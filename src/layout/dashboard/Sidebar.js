c58fc4a12d8ec0e6e358312d46cafa35100fb355
mdsaharshital <mdsaharshitol@gmail.com>
2023-04-29 23:49:52 +0600

    dashboard responsived

--- src/layout/dashboard/Sidebar.js
+++ src/layout/dashboard/Sidebar.js
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
+import { HiMenuAlt1 } from "react-icons/hi";
+import { IoClose } from "react-icons/io5";
 import { Link } from "react-router-dom";
 import { FaChevronLeft } from "react-icons/fa";
 import { useSelector } from "react-redux";
 const Sidebar = () => {
+  const [showMenu, setShowMenu] = useState(false);
   const {
     user: { role },
   } = useSelector((state) => state.auth);
@@ -11,40 +14,89 @@ const Sidebar = () => {
     { name: "My Posted Jobs", path: "my-posted-jobs" },
   ];
   const candidateMenu = [{ name: "Applied Job", path: "applied-job" }];
+
+  const menu = (
+    <>
+      {role === "employer" &&
+        employerMenu.map(({ name, path }, index) => (
+          <li key={index}>
+            <Link
+              className="hover:bg-primary hover:text-white bg-primary/10 transition-all w-full block p-2 lg:py-2 lg:px-3 rounded-full"
+              to={path}
+            >
+              {name}
+            </Link>
+          </li>
+        ))}
+      {role === "candidate" &&
+        candidateMenu.map(({ name, path }, index) => (
+          <li key={index}>
+            <Link
+              className="hover:bg-primary hover:text-white bg-primary/10 transition-all w-full block p-2 lg:py-2 lg:px-3 rounded-full"
+              to={path}
+            >
+              {name}
+            </Link>
+          </li>
+        ))}
+    </>
+  );
   return (
-    <div className="bg-primary/10 col-span-2 h-screen sticky top-0">
-      <ul className="flex flex-col gap-2 w-full h-full  p-3">
-        <div className="flex justify-between items-center text-primary my-1">
-          <Link to="/" className="flex items-center">
-            <FaChevronLeft />
-            <h1>Back</h1>
-          </Link>
-          <h1 className="text-xl">Dashboard</h1>
+    <>
+      <div className="bg-primary/10 col-span-3 lg:col-span-2 h-screen sticky top-0 hidden md:block">
+        <ul className="flex flex-col gap-2 w-full h-full  p-3">
+          <div className="block lg:flex justify-between items-center text-primary my-1">
+            <Link to="/" className="flex items-center mb-3 lg:mb-0">
+              <FaChevronLeft />
+              <h1>Back</h1>
+            </Link>
+            <h1 className="lg:text-[18px]">Dashboard</h1>
+          </div>
+          {menu}
+        </ul>
+      </div>
+      <div className="absolute right-2 top-5 md:hidden">
+        {showMenu ? (
+          <IoClose
+            className="text-black h-7 w-7 cursor-pointer"
+            onClick={() => setShowMenu(false)}
+          />
+        ) : (
+          <HiMenuAlt1
+            className="text-black h-7 w-7 cursor-pointer"
+            onClick={() => setShowMenu(true)}
+          />
+        )}
+      </div>
+      {showMenu && (
+        <div className="md:hidden z-[999]">
+          <ul className="w-[35vw] h-[100vh] bg-gray-900 text-white py-4">
+            {role === "employer" &&
+              employerMenu.map(({ name, path }, index) => (
+                <li key={index}>
+                  <Link
+                    className="bg-white text-black transition-all w-full block p-2 lg:py-2 lg:px-3 rounded-full"
+                    to={path}
+                  >
+                    {name}
+                  </Link>
+                </li>
+              ))}
+            {role === "candidate" &&
+              candidateMenu.map(({ name, path }, index) => (
+                <li key={index}>
+                  <Link
+                    className="bg-white text-black transition-all w-full block p-2 lg:py-2 lg:px-3 rounded-full"
+                    to={path}
+                  >
+                    {name}
+                  </Link>
+                </li>
+              ))}
+          </ul>
         </div>
-        {role === "employer" &&
-          employerMenu.map(({ name, path }, index) => (
-            <li key={index}>
-              <Link
-                className="hover:bg-primary hover:text-white bg-primary/10 transition-all w-full block py-2 px-3 rounded-full"
-                to={path}
-              >
-                {name}
-              </Link>
-            </li>
-          ))}
-        {role === "candidate" &&
-          candidateMenu.map(({ name, path }, index) => (
-            <li key={index}>
-              <Link
-                className="hover:bg-primary hover:text-white bg-primary/10 transition-all w-full block py-2 px-3 rounded-full"
-                to={path}
-              >
-                {name}
-              </Link>
-            </li>
-          ))}
-      </ul>
-    </div>
+      )}
+    </>
   );
 };
 
--- src/pages/candidateDashboard/AppliedJobs.js
+++ src/pages/candidateDashboard/AppliedJobs.js
@@ -1,7 +1,10 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
-import { useAppliedJobsQuery } from "../../features/job/jobSlice";
+import {
+  useAppliedJobsQuery,
+  useGetJobsQuery,
+} from "../../features/job/jobSlice";
 import { BsChevronRight } from "react-icons/bs";
 
 const AppliedJobs = () => {
@@ -10,44 +13,52 @@ const AppliedJobs = () => {
     user: { email },
   } = useSelector((state) => state.auth);
   const { data, isLoading } = useAppliedJobsQuery(email);
-
+  const { data: getJobsData, isLoading: getJobsLoading } = useGetJobsQuery();
+  // console.log("", getJobsData);
   if (isLoading) {
     return <p className="text-xl py-5">Loading.....</p>;
   }
-
+  const filteredData = getJobsData?.data.filter((data) =>
+    data.applicants.filter((newD) => newD.email === email)
+  );
+  console.log(filteredData);
   return (
     <div className="px-3">
       <h1 className="text-xl py-5">Applied jobs</h1>
-      <div className="grid grid-cols-2 gap-5 pb-5 ">
-        {data?.data?.map(({ _id, position, companyName }) => (
-          <div
-            key={_id}
-            className="border border-gray-300 shadow-md p-4 rounded-xl text-primary"
-          >
-            <div className="flex justify-between  text-primary ">
-              <div>
-                <p
-                  className="text-xl cursor-pointer hover:text-blue-500"
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-5 pb-5">
+        {getJobsData?.data
+          ?.filter((data) =>
+            data.applicants.filter((newD) => newD.email === email)
+          )
+          ?.map(({ _id, position, companyName }, index) => (
+            <div
+              key={_id}
+              className="border border-gray-300 shadow-md p-4 rounded-xl text-primary"
+            >
+              <div className="flex justify-between  text-primary ">
+                <div>
+                  <p
+                    className="text-xl cursor-pointer hover:text-blue-500"
+                    onClick={() => navigate(`/job-details/${_id}`)}
+                  >
+                    {index + 1}. {position}
+                  </p>
+                  <small className="text-primary/40 ">
+                    by{" "}
+                    <span className="font-semibold hover:text-primary cursor-pointer hover:underline transition-all">
+                      {companyName}
+                    </span>
+                  </small>
+                </div>
+                <button
+                  className=""
                   onClick={() => navigate(`/job-details/${_id}`)}
                 >
-                  {position}
-                </p>
-                <small className="text-primary/40 ">
-                  by{" "}
-                  <span className="font-semibold hover:text-primary cursor-pointer hover:underline transition-all">
-                    {companyName}
-                  </span>
-                </small>
+                  <BsChevronRight />
+                </button>
               </div>
-              <button
-                className=""
-                onClick={() => navigate(`/job-details/${_id}`)}
-              >
-                <BsChevronRight />
-              </button>
             </div>
-          </div>
-        ))}
+          ))}
       </div>
     </div>
   );
